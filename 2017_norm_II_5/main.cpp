2a1d02467c87b07081c35962a10b2738c8e4517a
sboda1985 <bodaszilard@gmail.com>
2019-10-08 07:24:57 +0300

    2017 norm II 5 added

---
+++ 2017_norm_II_5/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100][100];
+    int i, j, n, m;
+    cout << "n=";
+    cin >> n;
+    cout << "m=";
+    cin >> m;
+    for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            cout << "a[" << i << "][" << j << "]=";
+            cin >> a[i][j];
+        }
+    }
+    int x = a[n-1][m-1];
+    for(i=0; i<n; i++){
+        a[i][0] = x;
+        a[i][m-1] = x;
+    }
+
+    for(j=0; j<n; j++){
+        a[0][j] = x;
+        a[n-1][j] = x;
+    }
+
+    for(i=0; i<n; i++){
+        for(j=0; j<m; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
