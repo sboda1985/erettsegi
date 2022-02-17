1468cd2b73ff00018023c322049ad64d06c847e2
sboda1985 <bodaszilard@gmail.com>
2022-02-17 10:42:33 +0200

    Add files via upload

--- 2021_antr9_III_2/main.cpp
+++ 2021_antr9_III_2/main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    string szoveg;
-    getline(cin , szoveg);
-    int sz1=-1,sz2;
-    for(int i=0 ; i<szoveg.size() ;i++){
-        if(szoveg[i]==' '){
-            sz2=i;
+    int m=0;
+    string szoveg, mh="aeiou";
+    getline(cin,szoveg);
+    string szo = "";
+    for(int i=0 ; i<szoveg.size() ; i++)
+    {
+        if(szoveg[i]!=' ')
+        {
+            szo=szo + szoveg[i];
+        }
+        if (szoveg[i] == ' ' || i == szoveg.size()-1)
+        {
+            int stat[5] = {0,0,0,0,0};
+            for(int mhindex = 0; mhindex < mh.size(); mhindex++)
+            {
+                for(int szoindex = 0; szoindex < szo.size(); szoindex++)
+                {
+                    if (mh[mhindex] == szo[szoindex]) {
+                        stat[mhindex]++;
+                    }
+                }
+            }
+            int szamlalo = 0;
+            for(int index = 0; index < 5; index++) {
+                if (stat[index] > 0) {
+                    szamlalo++;
+                }
+            }
+            if (szamlalo == 1) {
+                cout << szo << endl;
+            }
+            szo= "";
         }
     }
     return 0;
