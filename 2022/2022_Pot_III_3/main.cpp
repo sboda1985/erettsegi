6a74f0a7098104cfec0d4a8475dc966ca4dbfebd
= <bodaszilard@gmail.com>
2024-05-22 09:37:19 +0300

    added new solutions

---
+++ 2022/2022_Pot_III_3/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+    ifstream in("bac.txt");
+    int x, y, nr = 1, max = 0, maxnr = 0;
+    in >> y;
+    while(in >> x){
+        if (x %2 == y % 2){
+            nr++;
+        } else {
+            if (nr == max) {
+                maxnr++;
+            }
+            if (nr > max){
+                max = nr;
+                maxnr = 1;
+            }
+            nr = 1;
+
+        }
+        y = x;
+    }
+    if (nr == max) {
+                maxnr++;
+            }
+            if (nr > max){
+                max = nr;
+                maxnr = 1;
+            }
+    cout << maxnr << " " << max << endl;
+    return 0;
+}
